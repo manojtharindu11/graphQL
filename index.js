9f20a229024def636e46212fe456856334a0ad90
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-01-11 10:25:12 +0530

    Add initial database structure and resolvers for games, reviews, and authors

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// Database
+import db from "./db.js";
+
 // Types
 import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        reviews() {
+            return db.reviews;
+        },
+        authors() {
+            return db.authors;
+        }
+    }
+}
 
 // Server setup
 const server = new ApolloServer({
