6deef85d5a5fad4d8cdd7763e5dcb8bebc6313f2
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-01-15 14:56:02 +0530

    Add updateGame mutation to allow updating existing games

--- index.js
+++ index.js
@@ -8,60 +8,71 @@ import db from "./_db.js";
 import { typeDefs } from "./schema.js";
 
 const resolvers = {
-    Query: {
-        games() {
-            return db.games;
-        },
-        game(_,args) {
-            return db.games.find(game => game.id === args.id);
-        },  
-        reviews() {
-            return db.reviews;
-        },
-        review(_,args) {
-            return db.reviews.find(review => review.id === args.id);
-
-        },
-        authors() {
-            return db.authors;
-        },
-        author(_,args) {
-            return db.authors.find(author => author.id === args.id);
-        }
+  Query: {
+    games() {
+      return db.games;
     },
-    Game: {
-        reviews(parent) {
-            return db.reviews.filter((review) => review.game_id === parent.id);
-        }
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
     },
-    Author: {
-        reviews(parent) {
-            return db.reviews.filter((review) => review.author_id === parent.id);
-        }
+    reviews() {
+      return db.reviews;
     },
-    Review: {
-        author(parent) {
-            return db.authors.find((author) => author.id === parent.author_id);
-        },
-        game(parent) {
-            return db.games.find((game) => game.id === parent.game_id);
-        }
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
     },
-    Mutation: {
-        addGame(_,args) {
-            let game = {
-                ...args.game,
-                id: Math.floor(Math.random() * 10000).toString()
-            }
-            db.games.push(game);
-            return game;
-        },
-        deleteGame(_,args) {
-            db.games = db.games.filter((game)=> game.id !== args.id);
-            return db.games;
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return game;
+    },
+    deleteGame(_, args) {
+      db.games = db.games.filter((game) => game.id !== args.id);
+      return db.games;
+    },
+    updateGame(_, args) {
+      let game = db.games.map((game) => {
+        if (game.id === args.id) {
+          return {
+            ...game,
+            ...args.game,
+          };
         }
-    }
-}
+        return game;
+      });
+      return db.game.find((game) => game.id === args.id);
+    },
+  },
+};
 
 // Server setup
 const server = new ApolloServer({
@@ -71,4 +82,4 @@ const server = new ApolloServer({
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
-console.log(`🚀 Server ready at port `,4000);
+console.log(`🚀 Server ready at port `, 4000);
